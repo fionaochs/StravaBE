cec0053b34950f6f9a98edec33f8afc47d30eb95
Fiona <fionalochs@gmail.com>
2020-11-15 17:11:20 -0800

    rec by athlete

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,18 +1,22 @@
 CREATE TABLE IF NOT EXIST ratings (
-  id serial PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
   athletes_id INT REFERENCES athletes (id),
   shoes_id INT REFERENCES shoes (id),
   stgars FLOAT
 )
 CREATE TABLE IF NOT EXIST athletes (
-  id serial PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
   name TEXT,
   weekly_mileage INT
-)gg
+)
 CREATE TABLE IF NOT EXIST shoes (
-  id serial PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
   terrain TEXT,
   model TEXT,
   avg_rating FLOAT,
   durable_miles INT
 )
+
+-- INSERT INTO ratings (
+
+-- )
