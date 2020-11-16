9e5c7c6f60d525627feb4646fceeb0186b04435a
Fiona <fionalochs@gmail.com>
2020-11-15 16:03:15 -0800

    avg calculated for ratings

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,16 +1,16 @@
 CREATE TABLE IF NOT EXIST ratings (
-  id PRIMARY KEY,
+  id serial PRIMARY KEY,
   athletes_id INT REFERENCES athletes (id),
   shoes_id INT REFERENCES shoes (id),
   stars FLOAT
 )
 CREATE TABLE IF NOT EXIST athletes (
-  id PRIMARY KEY,
+  id serial PRIMARY KEY,
   name TEXT,
   weekly_mileage INT
 )
 CREATE TABLE IF NOT EXIST shoes (
-  id PRIMARY KEY,
+  id serial PRIMARY KEY,
   terrain TEXT,
   model TEXT,
   avg_rating FLOAT,
