a0ccd5e2ec719eb60b79bac0e7ac765604830b3d
Fiona <fionalochs@gmail.com>
2020-11-16 19:06:55 -0800

    add avg stars

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,22 +1,19 @@
-CREATE TABLE IF NOT EXISTS ratings (
-  id INTEGER PRIMARY KEY NOT NULL,
-  athletes_id INT REFERENCES athletes (id),
-  shoes_id INT REFERENCES shoes (id),
-  stgars FLOAT
-)
-CREATE TABLE IF NOT EXISTS athletes (
-  id INTEGER PRIMARY KEY,
-  name TEXT,
-  weekly_mileage INT
-)
-CREATE TABLE IF NOT EXISTS shoes (
-  id INTEGER PRIMARY KEY,
-  terrain TEXT,
-  model TEXT,
-  avg_rating FLOAT,
-  durable_miles INT
-)
-
--- INSERT INTO ratings (
-
+-- CREATE TABLE IF NOT EXISTS ratings (
+--   id INTEGER PRIMARY KEY NOT NULL,
+--   athletes_id INT REFERENCES athletes (id),
+--   shoes_id INT REFERENCES shoes (id),
+--   stars FLOAT
 -- )
+-- CREATE TABLE IF NOT EXISTS athletes (
+--   id INTEGER PRIMARY KEY,
+--   name TEXT,
+--   weekly_mileage INT
+-- )
+-- CREATE TABLE IF NOT EXISTS shoes (
+--   id INTEGER PRIMARY KEY,
+--   terrain TEXT,
+--   model TEXT,
+--   avg_rating FLOAT,
+--   durable_miles INT
+-- )
+
